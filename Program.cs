d294f2f14b733b33c326410f87a646e29d59bf49
mario.lacerd <mario.lacerdti13@gmail.com>
2024-06-15 19:38:08 -0300

    Desafio Dio - DesafioProjetoHospedagem C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using DesafioProjetoHospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+// Cria a suíte
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+// Cria uma nova reserva, passando a suíte e os hóspedes
+Reserva reserva = new Reserva(diasReservados: 15);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+// Exibe a quantidade de hóspedes e o valor da diária
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor da diária: {suite.ValorDiaria:C}");
+Console.WriteLine($"Dias reservados: {reserva.DiasReservados}");
+if (reserva.DiasReservados >= 10)
+{
+    Console.WriteLine("Estadia igual ou superior a 10 dias ganha desconto de 10%!");
+}
+else { }
+Console.WriteLine($"Valor total: {reserva.CalcularValorDiaria():C}");
